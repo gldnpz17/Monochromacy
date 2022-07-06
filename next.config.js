6aa9e567ff4954219674dcd613eb76bae25a6a3c
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2022-07-06 22:10:48 +0700

    Fix for Next.js failing to create the standalone directory.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  experimental: {
+    outputStandalone: true,
+    outputFileTracingRoot: path.join(__dirname, '../../')
+  },
   reactStrictMode: true
 }
 
