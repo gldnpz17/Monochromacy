c1898ceaf4ba33ff0d88d13fcd78c54dcef88186
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2022-07-07 09:23:51 +0700

    Change hardcoded object detection service hostname.

--- api-requests/object-detection.js
+++ api-requests/object-detection.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const host = process.env.NEXT_PUBLIC_OBJECT_DETECTION_SERVICE_HOST
 
-const findObjects = async ({ form }) => (await axios.post(`${Boolean(host) ? host : "http://20.109.48.247:80"}/get-bounding-boxes`, form)).data
+const findObjects = async ({ form }) => (await axios.post(`${Boolean(host) ? host : "https://objectdetection.gldnpz.com"}/get-bounding-boxes`, form)).data
 
 export { findObjects }
